272225478a411c22d65626fdc74f1afaa67ac427
禾几海 <a@celess.cn>
2022-03-04 23:17:25 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <view class="index">
+    <canvas
+      type="2d"
+      class="canvas-demo"
+      :disableScroll="true"
+      @onTouchStart="onTouchStart"
+      @onTouchMove="onTouchMove"
+      @onTouchEnd="onTouchEnd"
+      @onTouchCancel="onTouchCancel"
+      @onLongTap="onLongTap"
+      @onError="onError"
+    >
+    </canvas>
+  </view>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  methods: {
+    onTouchStart(e) {
+      console.log('onTouchStart', e)
+    },
+    onTouchMove(e) {
+      console.log('onTouchMove', e)
+    },
+    onTouchEnd(e) {
+      console.log('onTouchEnd', e)
+    },
+    onTouchCancel(e) {
+      console.log('onTouchCancel', e)
+    },
+    onLongTap(e) {
+      console.log('onLongTap', e)
+    },
+    onError(e) {
+      console.log('onError', e)
+    },
+  }
+}
+</script>
+
+<style>
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
